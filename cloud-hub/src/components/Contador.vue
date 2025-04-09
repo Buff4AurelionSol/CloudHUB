9c66176b69bff995ad7bd228e675ded1582abd00
Deivis Guillen <noctis2001@gmail.com>
2025-04-09 13:59:51 -0400

    feat: add practices with the components 'Contador' and 'List '

---
+++ cloud-hub/src/components/Contador.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+  import {ref} from 'vue'
+
+  const iterator = ref(0)
+  const finalNumber = ref(0)
+
+  function plusIterator(){
+    iterator.value += finalNumber.value
+  }
+
+  function decrementIterator(){
+    iterator.value -= finalNumber.value
+  }
+
+  function handleClick(e){
+    e.preventDefault() as HtmlFormElement
+    const data = new FormData(e.target)
+    const number = data.get('numero')
+    console.log(`El numero es de tipo: ${typeof(number)}`)
+    finalNumber.value = parseInt(number)  
+  }
+
+  function clean(){
+    finalNumber.value = 0
+    iterator.value = 0 
+  }
+
+
+</script>
+
+<template>
+  <div class="main">
+    Prueba 
+    <form @submit="handleClick">
+      <input type="number" name="numero">
+      <button type="submit">Elegir numero</button>
+    </form>
+
+    <div class="buttons">
+      <button @click="plusIterator">Aumentar</button>
+      <button @click="decrementIterator">Decrementar</button>
+      <button @click="clean">Limpiar</button>
+    </div>
+    {{ iterator }}
+  </div>
+</template>
+
+<style scoped>
+  .main{
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .buttons{ 
+    display: flex; 
+  }
+
+</style>
---
+++ cloud-hub/src/components/List.vue
@@ -0,0 +1,57 @@
+<script setup>
+    import {ref} from 'vue'
+    const list = ref([])
+
+    function addNewTask(e){
+        e.preventDefault()
+        const data = new FormData(e.target)
+        const taskInput = data.get("task-input")
+        list.value.push(taskInput)
+        console.log(list.value)              
+    }
+    
+    function cleanList(){
+        list.value = []
+    }
+
+
+
+</script>
+
+<template>
+    <div class="list">
+        <h2>Lista de Tareas</h2>
+        <form @submit="addNewTask">
+            <input type="text" name="task-input">
+            <div class="buttons">
+                <button type="submit">Enviar</button>
+                <button type="button" @click="cleanList">Limpiar</button>
+            </div>
+        </form>
+        
+        <ul>
+            <li v-for="(item, index) in list" key="index">
+                {{index+1}}-{{item}} 
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .list{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center; 
+    }
+
+    input{ 
+     width: 150px;
+     height: 25px;
+    }
+
+    .buttons{
+        display: flex;
+        flex-direction: column;
+    }
+</style>
